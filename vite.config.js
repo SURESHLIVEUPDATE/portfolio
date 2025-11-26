069f57a719fe6dc9ccb37a0ef90cc89c62507399
Suresh <sureshc.sh2@gmail.com>
2025-11-26 18:47:27 +0530

    Update project from VS Code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: "/portfolio/", // <- must match your repo name
   plugins: [react()],
 });
